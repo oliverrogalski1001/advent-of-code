2fa4d1daf4c02299d90e780257d2fcab9f845d5d
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2022-12-03 15:57:12 -0600

    goofy

--- day3/day3-2.py
+++ day3/day3-2.py
@@ -5,10 +5,10 @@ with open("input") as f:
 points = 0
 for i in range(0, len(sacks), 3):
     a, b, c = sacks[i: i + 3]
-    char = set(a).intersection(set(b)).intersection(set(c)).pop()
+    char = set(a).intersection(set(b), set(c)).pop()
     if char.isupper():
         points += ord(char) - 38
     else:
         points += ord(char) - 96
-        
+
 print(points)
