a99fe445c28ba611f3340245ff3864ae0856bb37
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2022-12-12 14:32:06 -0600

    day 12

---
+++ day12/day12-2.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+with open("input") as f:
+    f = f.read().split("\n")
+    f = [list(i) for i in f]
+    for i in range(len(f)):
+        f[i] = [ord(i) - 97 for i in f[i]]
+    sr = sc = er = ec = 0
+    for i, arr in enumerate(f):
+        for j, val in enumerate(arr):
+            if val == -14:
+                f[i][j] = 0
+                sr, sc = i, j
+            if val == -28:
+                f[i][j] = 25
+                er, ec = i, j
+    visited = {(er, ec)}
+
+
+q = deque()
+q.append((er, ec, 0))
+while len(q) > 0:
+    r, c, count = q.popleft()
+    di = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    for d in di:
+        newR = r + d[0]
+        newC = c + d[1]
+        if newR >= len(f) or newC >= len(f[0]) or newR < 0 or newC < 0:
+            continue
+        if (newR, newC) in visited or f[newR][newC] - f[r][c] < -1:
+            continue
+        if f[r][c] == 0:
+            print(count)
+            exit()
+        visited.add((newR, newC))
+        q.append((newR, newC, count + 1))
