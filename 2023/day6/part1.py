18efc721059db91a4c0e4e9c2c4b5171b5f205c7
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2023-12-10 19:28:27 -0600

    day6 bug

--- 2023/day6/part1.py
+++ 2023/day6/part1.py
@@ -5,12 +5,16 @@ with open("input", "r") as f:
     times = [int(x.strip()) for x in lines[0][9:].split(" ") if x != ""]
     distances = [int(x.strip()) for x in lines[1][9:].split(" ") if x != ""]
 
-print(times)
-print(distances)
-
 record = 1
 for time, distance in zip(times, distances):
     a = math.ceil((-time + math.sqrt((time ** 2) - (4 * distance))) / (2 * -1))
     b = math.floor((-time - math.sqrt((time ** 2) - (4 * distance))) / (2 * -1))
-    record *= b - a + 1
+    print(b, a)
+    offset = 1
+    if (time * a) - (a**2) == distance:
+        offset -= 1
+    if (time * b) - (b**2) == distance:
+        offset -= 1
+    record *= b - a + offset
+
 print(record)
