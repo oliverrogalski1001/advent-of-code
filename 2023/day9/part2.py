fad49dde387f511d0bb0afd556e5ef06dcd5fc00
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2023-12-10 19:28:45 -0600

    day9 done

---
+++ 2023/day9/part2.py
@@ -0,0 +1,24 @@
+
+with open("input", "r") as f:
+    seqs = [[int(x.strip()) for x in line.split(" ")] for line in f.readlines()]
+
+def seq_diff(arr):
+    return [arr[i + 1] - arr[i] for i in range(len(arr) - 1)]
+
+sums = 0
+for seq in seqs:
+    diffs = [seq]
+    temp = seq
+
+    while temp.count(temp[0]) != len(temp):
+        temp_diff = seq_diff(temp)
+        temp = temp_diff
+        diffs.append(temp)
+
+    curr_num = diffs[-1][0]
+    for diff in reversed(diffs[:-1]):
+        curr_num = diff[0] - curr_num
+
+    sums += curr_num
+
+print(sums)
